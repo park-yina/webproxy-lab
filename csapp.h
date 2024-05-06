58ceb3f2c9493b6839ba5315167e3ea462aacb02
park-yina <111878820+park-yina@users.noreply.github.com>
2024-05-06 16:37:31 +0900

    Update csapp.h

--- csapp.h
+++ csapp.h
@@ -193,6 +193,8 @@ int open_listenfd(char *port);
 /* Wrappers for reentrant protocol-independent client/server helpers */
 int Open_clientfd(char *hostname, char *port);
 int Open_listenfd(char *port);
+void echo(int connfd)
+
 
 
 #endif /* __CSAPP_H__ */
