e283e4c3a7f85f22bf787708222afee2e83b466a
park-yina <111878820+park-yina@users.noreply.github.com>
2024-05-06 16:35:40 +0900

    Create echoclient.c

---
+++ netp/echoclient.c
@@ -0,0 +1,21 @@
+#include "csapp.h"
+int main(int argc, char** argv) {
+	int clientfd;
+	char* host, * port, buf[MAXLINE];
+	rio_t rio;
+	if (argc != 3) {
+		fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+		exit(0);
+	}
+	host = argv[1];
+	port = argv[2];
+	clientfd = Open_clientfd(host, port);
+	Rio_readinitb(&rio, clientfd);
+	While(Fgets * byf, MAXLINE, stdin != NULL) {
+		Rio_writen(clientfd, buf, strlen(buf));
+		Rio_readinitb(&rio, buf, MAXLINE);
+		Fputs(buf, stdout);
+	}
+	Close(clientfd);
+	exit(0);
+}
