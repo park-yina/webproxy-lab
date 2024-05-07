f8881e5f7659b9607bbad48896314deee0e91f40
park-yina <111878820+park-yina@users.noreply.github.com>
2024-05-07 10:26:29 +0900

    Update tiny.c

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -7,15 +7,62 @@
  *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
  */
 #include "csapp.h"
+#include "tiny.h"
+//TODO: 컴파일 에러를 막기 위한 선언부입니다.
+doit(int fd) {
+    void doit(int fd) {
+        int is_static;
+        struct stat sbuf;
+        char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+        char filename[MAXLINE], cgiargs[MAXLINE];
+        rio_t rio;
 
-void doit(int fd);
-void read_requesthdrs(rio_t *rp);
-int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
-void get_filetype(char *filename, char *filetype);
-void serve_dynamic(int fd, char *filename, char *cgiargs);
-void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
-                 char *longmsg);
+        /* 요청 라인 읽기 */
+        Rio_readinitb(&rio, fd);
+        if (!Rio_readlineb(&rio, buf, MAXLINE))
+            return;
+
+        /* 요청 라인 출력 */
+        printf("%s", buf);
+
+        /* 요청 라인 파싱 */
+        sscanf(buf, "%s %s %s", method, uri, version);
+        if (strcasecmp(method, "GET")) {
+            /* 메소드가 GET이 아닌 경우 오류 메시지 제공 */
+            clienterror(fd, method, "501", "Not Implemented", "Tiny는 이 메소드를 구현하지 않습니다.");
+            return;
+        }
+
+        /* 요청 헤더 읽기 */
+        read_requesthdrs(&rio);
+
+        /* GET 요청에서 URI 파싱 */
+        is_static = parse_uri(uri, filename, cgiargs);
+        if (stat(filename, &sbuf) < 0) {
+            /* 파일을 찾을 수 없는 경우 오류 메시지 제공 */
+            clienterror(fd, filename, "404", "Not found", "Tiny가 이 파일을 찾을 수 없습니다.");
+            return;
+        }
+
+        if (is_static) { /* 정적 컨텐츠 제공 */
+            if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+                /* 파일을 읽을 수 없는 경우 오류 메시지 제공 */
+                clienterror(fd, filename, "403", "Forbidden", "Tiny가 이 파일을 읽을 수 없습니다.");
+                return;
+            }
+            serve_static(fd, filename, sbuf.st_size);
+        }
+        else { /* 동적 컨텐츠 제공 */
+            if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+                /* CGI 프로그램을 실행할 수 없는 경우 오류 메시지 제공 */
+                clienterror(fd, filename, "403", "Forbidden", "Tiny가 이 CGI 프로그램을 실행할 수 없습니다.");
+                return;
+            }
+            serve_dynamic(fd, filename, cgiargs);
+        }
+    }
+
+}
 
 int main(int argc, char **argv) {
   int listenfd, connfd;
